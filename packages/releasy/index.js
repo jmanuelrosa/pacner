5a176d619e8bed3d96028e57485159c24539cf73
José Manuel Rosa <josemanuel.rosamoncayo+github@gmail.com>
2021-08-05 09:07:16 +0200

    feat: test
    BREAKING CHANGE: force update

--- packages/releasy/index.js
+++ packages/releasy/index.js
@@ -2,7 +2,9 @@ import bump from 'conventional-recommended-bump'
 
 export function cli () {
   bump({
-    preset: 'conventionalcommits'
+    preset: 'conventionalcommits',
+    path: "/home/jmanuelrosa/Developer/personal/pacner/packages/eslint-config",
+    lernaPackage: "@pacner/eslint-config",
   }, (error, recommendation) => {
     console.log(recommendation.releaseType);
   });
